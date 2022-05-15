3894ca79c1b823b069cd6caa23b86def10aff66a
Lily Smart <hello@neyoa.me>
2022-05-15 17:38:23 +0100

    Attempt to fix logger crash

--- src/Logger.js
+++ src/Logger.js
@@ -5,35 +5,35 @@ class Logger {
    * @param {string} message
    */
   discord(message) {
-    return console.log(chalk.bgMagenta.black(`[${this.getCurrentTime()}] Discord >`) + ' ' + chalk.magenta(message))
+    return console.log(chalk.bgMagenta.black(`[${Logger.getCurrentTime()}] Discord >`) + ' ' + chalk.magenta(message))
   }
 
   /**
    * @param {string} message
    */
   minecraft(message) {
-    return console.log(chalk.bgGreenBright.black(`[${this.getCurrentTime()}] Minecraft >`) + ' ' + chalk.greenBright(message))
+    return console.log(chalk.bgGreenBright.black(`[${Logger.getCurrentTime()}] Minecraft >`) + ' ' + chalk.greenBright(message))
   }
 
   /**
    * @param {string} message
    */
   express(message) {
-    return console.log(chalk.bgCyan.black(`[${this.getCurrentTime()}] Express >`) + ' ' + chalk.cyan(message))
+    return console.log(chalk.bgCyan.black(`[${Logger.getCurrentTime()}] Express >`) + ' ' + chalk.cyan(message))
   }
 
   /**
    * @param {string} message
    */
   warn(message) {
-    return console.log(chalk.bgYellow.black(`[${this.getCurrentTime()}] Warning >`) + ' ' + chalk.yellow(message))
+    return console.log(chalk.bgYellow.black(`[${Logger.getCurrentTime()}] Warning >`) + ' ' + chalk.yellow(message))
   }
 
   /**
    * @param {string} message
    */
   error(message) {
-    return console.log(chalk.bgRedBright.black(`[${this.getCurrentTime()}] Error >`) + ' ' + chalk.redBright(message))
+    return console.log(chalk.bgRedBright.black(`[${Logger.getCurrentTime()}] Error >`) + ' ' + chalk.redBright(message))
   }
 
   /**
@@ -41,10 +41,10 @@ class Logger {
    * @param {string} location
    */
   broadcast(message, location) {
-    return console.log(chalk.inverse(`[${this.getCurrentTime()}] ${location} Broadcast >`) + ' ' + message)
+    return console.log(chalk.inverse(`[${Logger.getCurrentTime()}] ${location} Broadcast >`) + ' ' + message)
   }
 
-  getCurrentTime() {
+  static getCurrentTime() {
     return new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false })
   }
 }
